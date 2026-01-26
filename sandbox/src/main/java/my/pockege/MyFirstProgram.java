fc1f273551e8f340f85beabd57af451b502a8139
Renata2990 <mukhtarova.renata@gmail.com>
2026-01-26 10:35:51 +0500

    refactor code

--- sandbox/src/main/java/my/pockege/MyFirstProgram.java
+++ sandbox/src/main/java/my/pockege/MyFirstProgram.java
@@ -2,7 +2,7 @@ package my.pockege;
 
 public class MyFirstProgram {
 
-	public static void main(String[] args) {
-	  System.out.println("Hello, World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
